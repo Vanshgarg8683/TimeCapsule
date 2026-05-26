c339bbc3a65a75d80e2e74bba408f55c53a3866f
= <vg9233384@gmail.com>
2026-05-26 17:53:40 +0530

    postgres setup

--- TIMECAPSULE/settings.py
+++ TIMECAPSULE/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'TIMECAPSULE.wsgi.application'
 
 # DATABASE
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config("DATABASE_URL")
+    )
 }
 
 
